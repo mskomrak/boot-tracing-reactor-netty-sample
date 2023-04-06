3d24d010fc4186418951d4ba92b068463a0d8603
Mislav Skomrak <mislav.skomrak@gmail.com>
2023-04-06 17:21:30 +0200

    Add Logbook

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Steps to reproduce:
 1. run the service, run Zipkin using the [docker-compose.yml](stack/docker-compose.yml)
-2. execute `curl -X GET http://127.0.0.1:8080/data` or `curl -X POST http://127.0.0.1:8080/data`
-3. wiretap logs are missing tracing data
+2. execute `curl -X POST http://127.0.0.1:8080/data`
+3. observe the logs - traceId is present
+   * logbook logger (`org.zalando.logbook.Logbook`) statements present for both read and write along with tracing data
+   * reactor-netty log (`reactor.netty.http.client.HttpClient`) statement present for all events along with tracing data
+4. execute another `curl -X POST http://127.0.0.1:8080/data` (almost immediately following the previous one)
+   * reactor-netty log (`reactor.netty.http.client.HttpClient`) statement present, but traceId missing for some events (e.g. flush, read)
+   * logbook logger (`org.zalando.logbook.Logbook`) statements missing
+5. wait some time (e.g. several minutes), execute another `curl -X POST http://127.0.0.1:8080/data`
+    * logbook logger (`org.zalando.logbook.Logbook`) statements present for both read and write along with tracing data
+    * reactor-netty logs (`reactor.netty.http.client.HttpClient`) statement present for all events along with tracing data
--- src/main/java/com/example/demo/config/HttpConfiguration.java
+++ src/main/java/com/example/demo/config/HttpConfiguration.java
@@ -3,25 +3,31 @@ package com.example.demo.config;
 import io.netty.handler.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.netty.LogbookClientHandler;
 import reactor.netty.NettyPipeline;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
+import java.nio.charset.Charset;
 import java.util.function.Function;
 
 @Configuration(proxyBeanMethods = false)
 public class HttpConfiguration {
 
     @Bean
-    HttpClient httpClient() {
+    HttpClient httpClient(Logbook logbook) {
         return HttpClient.create()
                 .metrics(true, Function.identity())
                 .doOnChannelInit((obs, ch, addr) ->
                         ch.pipeline()
                                 .addFirst(TracingChannelInboundHandler.NAME, TracingChannelInboundHandler.INSTANCE)
                                 .addBefore(NettyPipeline.HttpMetricsHandler, TracingChannelOutboundHandler.NAME,
-                                        TracingChannelOutboundHandler.INSTANCE))
-                .wiretap("reactor.netty.http.client.HttpClient", LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
+                                        TracingChannelOutboundHandler.INSTANCE)
+                                .addBefore(TracingChannelOutboundHandler.NAME, "logbook", new LogbookClientHandler(logbook)))
+                .doOnConnected(c -> c.addHandlerFirst(AdvancedByteBufFormat.TEXTUAL.toLoggingHandler(
+                        "reactor.netty.http.client.HttpClient", LogLevel.INFO, Charset.defaultCharset())))
+                ;
     }
 
 }
